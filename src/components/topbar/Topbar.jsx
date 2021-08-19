14906fd1a890293b3d3137ae2168f14f4743b88e
serendipity-harpoon <sunhaopeng2016@gmail.com>
2021-08-19 15:43:47 -0400

    animated menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Topbar.scss";
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import EmailIcon from '@material-ui/icons/Email';
 
 const Topbar = function(){
+  const [active, setActive] = useState(false)
+
   return (
-    <div className="topbar">
+    <div className={"topbar " + (active && "active")}>
       <div className="wapper">
         <div className="left">
           <a href="#intro" className="logo">HAOPENG</a>
@@ -17,9 +19,14 @@ const Topbar = function(){
             <EmailIcon className="icon"/>
             <span>SUNHP2016@outlook.com</span>
           </div>
+          <h3 className="about">About</h3>
         </div>
         <div className="right">
-          <h3>About</h3>
+          <div className="hamburger" onClick={() => setActive(!active)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
     </div>
