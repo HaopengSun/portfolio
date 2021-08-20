2d4bd22c1bd14246883066ad33efa17b89dd0444
serendipity-harpoon <sunhaopeng2016@gmail.com>
2021-08-20 13:19:55 -0400

    edit about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import "../topbar/Topbar";
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import EmailIcon from '@material-ui/icons/Email';
+import { Link } from "react-router-dom"
 
 const About = function(){
   return (
     <div>
-      About
+      <div className="wapper">
+        <div className="left">
+          <a href="/" className="logo">HAOPENG</a>
+          <div className="itemcontainer">
+            <PermIdentityIcon className="icon"/>
+            <span>514-632-3066</span>
+          </div>
+          <div className="itemcontainer">
+            <EmailIcon className="icon"/>
+            <span>SUNHP2016@outlook.com</span>
+          </div>
+          <Link className="about" to="/about">About</Link>
+        </div>
+      </div>
     </div>
   )
 }
