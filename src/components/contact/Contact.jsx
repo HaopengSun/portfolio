378d2d5e36d71a72c5be0ce7d3073ca98d4c96b3
serendipity-harpoon <sunhaopeng2016@gmail.com>
2021-08-21 11:03:47 -0400

    add emailJS to auto reply message

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,33 @@
 import { useState } from "react";
 import "./Contact.scss";
+import emailjs from 'emailjs-com';
 
 export default function Contact() {
-  const [message, setMessage] = useState(false);
+  const [send, setSend] = useState(false);
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleChange1 = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handleChange2 = (event) => {
+    setMessage(event.target.value);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setMessage(true);
+    setSend(true);
+    console.log(email);
+    console.log(message);
+    emailjs.sendForm('service_5cq8jnb', 'template_yrdbe9h', e.target, 'user_qBoTLA9q7oLwjp7wCoIis')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
   };
+
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -16,10 +36,10 @@ export default function Contact() {
       <div className="right">
         <h2>Contact.</h2>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
+          <input type="text" placeholder="Email" name="email" onChange={handleChange1}/>
+          <textarea placeholder="Message" name="message" onChange={handleChange2}></textarea>
           <button type="submit">Send</button>
-          {message && <span>Thanks, I'll reply ASAP :)</span>}
+          {send && <span>Thanks, I'll reply ASAP :)</span>}
         </form>
       </div>
     </div>
